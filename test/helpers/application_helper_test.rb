ed80490ee2118543404e7dd9461f01e83bf23a18
Ana Luiza Cunha <ana@ccs.neu.edu>
2015-11-23 16:38:11 +0000

    Test signup page title

---
+++ test/helpers/application_helper_test.rb
@@ -0,0 +1,10 @@
+require 'test_helper'
+
+class ApplicationHelperTest < ActionView::TestCase
+  
+  test "full title helper" do
+    assert_equal full_title,            'iLike App'
+    assert_equal full_title("Help"),    'Help | iLike App'
+  end
+  
+end
