c29f490e60e056c1857b6d4b26bbb560abb9a1e2
Ana Luiza Cunha <ana@ccs.neu.edu>
2015-11-23 04:19:24 +0000

    Add static pages (Home, About)

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,9 +1,17 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | iLike App"
+  end
+  
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title", "About | iLike App"
   end
 
 end
