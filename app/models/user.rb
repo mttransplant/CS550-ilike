853e70eb135197385cad947b01f1f89f6aab082e
Ana Luiza Cunha <ana@ccs.neu.edu>
2015-11-23 20:19:12 +0000

    Add User model with validations

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ActiveRecord::Base
+  
+  validates :first_name, presence: true, length: { maximum: 50 }
+  validates :last_name, presence: true, length: { maximum: 50 }
+  
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence:   true, length: { maximum: 255 },
+                    uniqueness: { case_sensitive: false },
+                    format:     { with: VALID_EMAIL_REGEX }
+                    
+  
+end
