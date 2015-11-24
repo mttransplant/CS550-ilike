97c384836a4deed6f5d4d3275a87680c975fed46
Ana Luiza Cunha <ana@ccs.neu.edu>
2015-11-24 18:50:03 +0000

    Show Users action

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  
-  get 'users/new'
 
   root             'static_pages#home'
+  
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
   get 'signup'  => 'users#new'
+  
+  resources :users
 
 end
