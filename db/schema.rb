214f7c2cb0f80d5ef35be295a892cb4c942e9298
Ana Luiza Cunha <ana@ccs.neu.edu>
2015-11-24 13:25:13 +0000

    Create database index on user email

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151123165842) do
+ActiveRecord::Schema.define(version: 20151123205412) do
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
@@ -20,5 +20,7 @@ ActiveRecord::Schema.define(version: 20151123165842) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
