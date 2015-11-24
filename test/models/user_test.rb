80555f77bd9270e2af9314e920a50d057f904809
Ana Luiza Cunha <ana@ccs.neu.edu>
2015-11-24 14:16:37 +0000

    Add secure password to User model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,11 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
   
   def setup
-    @user = User.new(first_name: "Michael", last_name: "Weintraub", email: "m.weintraub@northeastern.edu")
+    @user = User.new( first_name:             "Michael", 
+                      last_name:              "Weintraub", 
+                      email:                  "m.weintraub@northeastern.edu",
+                      password:               "northeastern",
+                      password_confirmation:  "northeastern")
   end
   
   test "should be valid" do
@@ -50,7 +54,7 @@ class UserTest < ActiveSupport::TestCase
   end
   
   test "email validation should reject invalid addresses" do
-    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example. foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example. foo@bar_baz.com foo@bar+baz.com, foo@bar..com]
     
     invalid_addresses.each do |invalid_address|
       @user.email = invalid_address
@@ -63,6 +67,23 @@ class UserTest < ActiveSupport::TestCase
     duplicate_user.email = @user.email.upcase
     @user.save
     assert_not duplicate_user.valid?
+  end
+  
+  test "email address should be saved as lower-case" do
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+  
+  test "password should be present (nonblank)" do
+    @user.password = @user.password_confirmation = ' ' * 6
+    assert_not @user.valid?
+  end
+  
+  test "password should have a minimium lenght" do
+    @user.password = @user.password_confirmation = 'a' * 5
+    assert_not @user.valid?
   end
   
 end
