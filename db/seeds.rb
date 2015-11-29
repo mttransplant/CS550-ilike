6592e1c0c844d52f8df31724f32109d3b13ab50c
Ana Luiza Cunha <ana@ccs.neu.edu>
2015-11-29 20:43:29 +0000

    Finish user edit, update, index, and destroy actions

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,30 +2,34 @@ User.create!(first_name:  "Ana",
               last_name:   "Cunha",
               email:       "ana@ccs.neu.edu",
               password:              "password",
-              password_confirmation: "password")
+              password_confirmation: "password",
+              admin: true)
 
 User.create!(first_name:  "Shiva",
               last_name:   "Chaturvedi",
               email:       "shivac@ccs.neu.edu",
               password:              "password",
-              password_confirmation: "password")
+              password_confirmation: "password",
+              admin: true)
 
 User.create!(first_name:  "Adam",
               last_name:   "Smith",
               email:       "apsmith@ccs.neu.edu",
               password:              "password",
-              password_confirmation: "password")
+              password_confirmation: "password",
+              admin: true)
 
 User.create!(first_name:  "Niharika",
               last_name:   "Sharma",
               email:       "nhrshm10@ccs.neu.edu",
               password:              "password",
-              password_confirmation: "password")
+              password_confirmation: "password",
+              admin: true)
 
 96.times do |n|
   first_name  = Faker::Name.first_name
   last_name   = Faker::Name.last_name
-  email = "#{first_name}.#{last_name}.#{n}@ilike.com"
+  email = Faker::Internet.safe_email(first_name + ' ' + last_name)
   password = "password"
   User.create!(first_name:  first_name,
                 last_name:   last_name,
