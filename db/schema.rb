be4f89a508fbccf051fdb0a891791f823d9117c3
Ana Luiza Cunha <ana@ccs.neu.edu>
2015-12-02 20:13:59 +0000

    Add Location model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151129195346) do
+ActiveRecord::Schema.define(version: 20151202200824) do
+
+  create_table "locations", force: :cascade do |t|
+    t.string   "name"
+    t.float    "latitude"
+    t.float    "longitude"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
