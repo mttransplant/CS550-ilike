5300ef98d5cd433448471322cc5adca4d31088f3
mttransplant <apsmith@ccs.neu.edu>
2015-12-07 00:52:45 +0000

    rolled back event images

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -2,8 +2,10 @@ module EventsHelper
   require 'open-uri'
   
   def event_image_generated
-    event_url = "http://dummyimage.com/200x200/000/fff.png&text=#{@event_id}"
-    image_tag(event_url, alt: "Event #{@event_id}", class: "event_image")
+
+    event_url = "http://dummyimage.com/200x200/000/fff.png&text=#{params[:id]}"
+
+    image_tag(event_url, size: "200x200", alt: "Event #{params[:id]}", class: "event_image")
   end
 
 
